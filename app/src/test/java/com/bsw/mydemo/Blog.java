714dec34c7d4a1032e7fb1c8411b74982d26dcf6
leiming <mengchun19930211>
2019-11-07 17:40:45 +0800

    Junit的java Test添加非Java的SDK包

--- app/src/test/java/com/bsw/mydemo/Blog.java
+++ app/src/test/java/com/bsw/mydemo/Blog.java
@@ -1,10 +1,37 @@
 package com.bsw.mydemo;
 
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class Blog {
     @Test
-    public void doSomething() {
+    public void jsonTest() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("aBoolean", true);
+            jsonObject.put("string", "hasController");
+            jsonObject.put("param", new Param());
+
+            MyChange change = new Gson().fromJson(jsonObject.toString(), MyChange.class);
+            System.out.println("转换成功   \n****************************\naBoolean = " + change.aBoolean
+                    + " \nstring = " + change.string
+                    + " \nparam = " + change.param
+                    + "\n****************************");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyChange {
+        boolean aBoolean;
+        boolean string = true;
+        boolean param = true;
+    }
+
+    private class Param {
 
     }
 }
